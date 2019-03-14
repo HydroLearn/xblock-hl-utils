b8d5b316c504e5e915714d9e91f4c1470439fdd4
crivet <c.rivet@louisiana.edu>
2019-03-14 08:53:21 -0500

    importing some potential additions from other hl-xblocks

--- hl_utils/hl_utils.py
+++ hl_utils/hl_utils.py
@@ -4,6 +4,32 @@ from xblockutils.resources import ResourceLoader
 loader = ResourceLoader(__name__)
 
 
+
+# currently UNUSED until better implemented
+class StudioModalFixMixin(object):
+
+    def studio_view(self, context=None):
+        """
+            The studio view of the xblock
+
+            return: a web fragment containing the necessary styles/scripts/templates
+                for rendering the xblock editor in the cms xblock modal window.
+
+
+        """
+        # THIS TECHINCALLY CAN HAPPEN AS A MIXIN (expected to just do, not inherit)
+        # fragment = super(StudioModalFixMixin, self).studio_view(context)
+        fragment = Fragment()
+
+        # add in the styling/script corrections for HL xblock component modals
+        fragment.add_css(loader.load_unicode('static/css/modal-styling.css'))
+        fragment.add_javascript(loader.load_unicode('static/js/StudioModalFix.js'))
+
+        fragment.initialize_js('StudioModalFix_script')
+
+        return fragment
+
+
 class HLXblockModalHelperMixin(object):
     """
         mixin to add modal tab definitions with associated templates
